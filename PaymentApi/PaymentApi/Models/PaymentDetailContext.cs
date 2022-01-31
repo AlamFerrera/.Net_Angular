442d0ae9d02550f1e257fe67dd41aa994af4b665
Alam Ferrera <alamferrera@gmail.com>
2022-01-31 10:42:14 -0400

    start
    start

---
+++ PaymentApi/PaymentApi/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+    }
+}
